7dc83b907532840c54524441e31a73496cadf4ff
valenpelaez99 <valenpelaez99@gmail.com>
2022-12-22 00:24:11 -0500

    First commit. Create project

---
+++ retofinal/src/main/java/com/reto/retofinal/RetofinalApplication.java
@@ -0,0 +1,13 @@
+package com.reto.retofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetofinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetofinalApplication.class, args);
+	}
+
+}
---
+++ retofinal/src/test/java/com/reto/retofinal/RetofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.retofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
